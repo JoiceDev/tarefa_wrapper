bf080c91d941db79f4419fa93663dfa8ac6956d1
Joice Gonçalves <joice.silva.s2@hotmail.com>
2024-12-27 12:44:39 -0300

    first comit

---
+++ src/tarefa_wrapper/TarefaWrapper.java
@@ -0,0 +1,16 @@
+package tarefa_wrapper;
+
+public class TarefaWrapper {
+
+	public static void main(String[] args) {
+
+		long number = 12345678;
+		
+		Long convertNumber = number;
+		
+		System.out.println("Número primitivo (long): " + number);
+		System.out.println("Número como wrapper (Long): " + convertNumber);
+		
+	}
+
+}
